94af809ac4d4ec7393baf75e458880a11aefd926
Marta-678 <martaroldan972@gmail.com>
2026-05-01 22:35:13 +0200

    rutas de proyectos

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,6 +1,15 @@
 import {Router} from 'express';
+import { authMiddleware } from './middlewares/authMiddleware.js';
+import { validate } from '../middleware/validate.js';
+import { createProject, updateProject } from './controllers/project.controller.js';
+import { createProjectValidator, updateProjectValidator } from './validators/project.validator.js';
+
 
 const projectRoutes = Router();
+projectRoutes.use(authMiddleware);
+
+projectRoutes.post('/', validate(createProjectValidator), createProject);
+projectRoutes.put('/:id', validate(updateProjectValidator), updateProject);
 
 
 
